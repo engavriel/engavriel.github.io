b41bb4a2cb51f241d02413294569dbeb1fba3fcc
engavriel <engavriel@gmail.com>
2022-09-25 19:13:35 +0000

    Update file(s) "/." from "engavriel/online-resume"

---
+++ resume/js/main.js
@@ -0,0 +1,14 @@
+$('.cmd').click(function () {
+    alert('ok');
+    // var doc = new jsPDF();
+    // var specialElementHandlers = {
+    //     '#editor': function (element, renderer) {
+    //         return true;
+    //     }
+    // };
+    // doc.fromHTML($('#content').html(), 15, 15, {
+    //     'width': 170,
+    //     'elementHandlers': specialElementHandlers
+    // });
+    // doc.save('sample-file.pdf');
+});
